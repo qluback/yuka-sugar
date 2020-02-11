97f655077153528fe5d528d06276b648fd716348
Quentin LUBACK <quentin.luback@eemi.com>
2020-02-11 14:46:15 +0100

    Scan QR Code

--- Yuka Sugar/ViewController.swift
+++ Yuka Sugar/ViewController.swift
@@ -9,13 +9,55 @@
 import UIKit
 import SceneKit
 import ARKit
+import AVFoundation
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        var scanDone: Bool = false
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -30,8 +72,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.scene = scene
     }
     
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+        }
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
@@ -39,12 +92,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
         
-        // Pause the view's session
         sceneView.session.pause()
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue: String = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        
+    }
+
+    func found(code: String) {
+        let alert = UIAlertController(title: "Yuka Sugar", message: "Ce produit contient \(code)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Afficher les sucres", style: .default, handler: nil
+        ))
+        alert.addAction(UIAlertAction(title: "Annuler", style: .default, handler: nil
+        ))
+        present(alert, animated: true, completion: nil)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -58,8 +144,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func createBall(position : SCNVector3) {
-        var ballShape = SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0)
-        var ballNode = SCNNode(geometry: ballShape)
+        let ballShape = SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0)
+        let ballNode = SCNNode(geometry: ballShape)
         ballNode.position = position
         sceneView.scene.rootNode.addChildNode(ballNode)
     }
